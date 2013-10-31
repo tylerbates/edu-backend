ac8e199731ef3e7dac722abb5561a1e9714e1747
tylerbates <tylerbates098@gmail.com>
2013-10-31 14:54:55 +0400

    Merge remote-tracking branch 'remotes/ogetto/master'
    Conflicts:
    	Vagrantfile

--- src/ProductCollection.php
+++ src/ProductCollection.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tylerbates
- * Date: 30.10.13
- * Time: 3:29
- */ 
+require_once 'Product.php';
+
+class ProductCollection
+{
+    public function __construct(array $data)
+    {
+        $this->_data = $data;
+    }
+
+    public function getProducts()
+    {
+        if(isset($this->_offset)){
+            $offset_products = array_slice($this->_data,$this->_offset);
+            return $offset_products;
+        }
+        return $this->_data;
+    }
+
+    public function getSize()
+    {
+        return isset($this->_limit) ? $this->_limit : count($this->_data);
+    }
+
+    public function limit($value)
+    {
+        $this->_limit = $value;
+    }
+
+    public function offset($value)
+    {
+        $this->_offset = $value;
+    }
+
+    private $_data;
+    private $_limit;
+    private $_offset;
+}
--- tests/ProductCollection.php
+++ tests/ProductCollection.php
@@ -1,7 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tylerbates
- * Date: 30.10.13
- * Time: 3:35
- */ 
+require_once __DIR__ . '/../src/ProductCollection.php';
+
+$products = new ProductCollection([new Product(['sku'=>'foo']),new Product(['sku'=>'bar'])]);
+if (assert($products->getProducts() == [new Product(['sku'=>'foo']),new Product(['sku'=>'bar'])],'Returns products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product(['sku'=>'bar']),new Product(['sku'=>'foo'])]);
+if (assert($products->getProducts() == [new Product(['sku'=>'bar']),new Product(['sku'=>'foo'])],'Returns products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product([]),new Product([])]);
+if (assert($products->getSize() == 2,'Returns number of products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product([])]);
+if (assert($products->getSize() == 1,'Returns number of products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product(['sku'=>'foo']),new Product(['sku'=>'bar'])]);
+$products->limit(1);
+if (assert($products->getProducts() == [new Product(['sku'=>'foo']),new Product(['sku'=>'bar'])] && $products->getSize() == 1,'Returns limited products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product(['sku'=>'foo']),new Product(['sku'=>'bar'])]);
+$products->limit(2);
+if (assert($products->getProducts() == [new Product(['sku'=>'foo']),new Product(['sku'=>'bar'])] && $products->getSize() == 2,'Returns limited products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product(['sku'=>'foo']),new Product(['sku'=>'bar']),new Product(['sku'=>'baz'])]);
+$products->offset(0);
+if (assert($products->getProducts() == [new Product(['sku'=>'foo']),new Product(['sku'=>'bar']),new Product(['sku'=>'baz'])],'Returns offset products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product(['sku'=>'foo']),new Product(['sku'=>'bar']),new Product(['sku'=>'baz'])]);
+$products->offset(1);
+if (assert($products->getProducts() == [new Product(['sku'=>'bar']),new Product(['sku'=>'baz'])],'Returns products which have been initialized')) {
+    echo '.';
+}
+
+$products = new ProductCollection([new Product(['sku'=>'foo']),new Product(['sku'=>'bar']),new Product(['sku'=>'baz'])]);
+$products->offset(2);
+if (assert($products->getProducts() == [new Product(['sku'=>'baz'])],'Returns products which have been initialized')) {
+    echo '.';
+}
+
+echo "\n";
