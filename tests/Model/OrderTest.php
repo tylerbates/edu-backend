88ffeb15864db19a5f0569ba031a8336c1b62f40
tylerbates <tylerbates098@gmail.com>
2014-01-18 17:49:21 +0400

    add test for order

--- tests/Model/OrderTest.php
+++ tests/Model/OrderTest.php
@@ -1,8 +1,41 @@
 <?php
 namespace Test\Model;
 
+use App\Model\Customer;
+use App\Model\ModelView;
+use App\Model\Order;
+
 class OrderTest extends \PHPUnit_Framework_TestCase
 {
+    public function testSetsParams()
+    {
+        $resource = $this->getMock('App\Model\Resource\IResourceEntity');
 
+        $order = new Order(
+            [],
+            $resource,
+            new \Zend\Mail\Transport\Smtp(),
+            new Customer([],$resource),
+            new \Zend\Mail\Message(),
+            new ModelView('','','','',[])
+        );
+        $order->setCustomerId(1);
+        $order->setShippingMethod('fixed');
+        $order->setPaymentMethod('courier');
+        $order->setAddress('ololo');
+        $order->setItems('1,2,3');
+        $order->setSubtotal(100);
+        $order->setShipping(100);
+        $order->setGrandTotal(200);
+
+        $this->assertEquals(1,$order->getData('customer_id'));
+        $this->assertEquals('fixed',$order->getData('shipping_method'));
+        $this->assertEquals('courier',$order->getData('payment_method'));
+        $this->assertEquals('ololo',$order->getData('address'));
+        $this->assertEquals('1,2,3',$order->getData('items'));
+        $this->assertEquals(100,$order->getData('subtotal'));
+        $this->assertEquals(100,$order->getData('shipping'));
+        $this->assertEquals(200,$order->getData('grand_total'));
+    }
 }
  
